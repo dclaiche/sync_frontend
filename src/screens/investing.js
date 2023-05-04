1abc5aa25f2743b93ab84aa4d06c8a10e9007725
Devon <dclaiche@hotmail.com>
2023-05-04 15:44:12 -0700

    Finished graph to good state

--- src/screens/investing.js
+++ src/screens/investing.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, useRef  } from 'react';
 import { LineChart } from 'react-native-wagmi-charts';
 import {
   StyleSheet,
@@ -10,195 +10,248 @@ import {
   SafeAreaView,
   StatusBar
 } from 'react-native';
-import moment from 'moment';
 import * as haptics from 'expo-haptics';
 import axios from 'axios';
 import Store from '../models/secureStore';
+import { LongPressGestureHandler, State } from 'react-native-gesture-handler';
+
 
 const Investing = () => {
-  const [timeframe, setTimeframe] = useState('1M');
-  const [buyingPower, setBuyingPower] = useState(0);
-  const [equity, setEquity] = useState(parseFloat(0.00));
-  const [baseValue, setBaseValue] = useState(0.00);
+  const [period, setPeriod] = useState('1W');
+  const [equity, setEquity] = useState(0);
   const [data, setData] = useState([]);
   const [LineChartColor, setLineChartColor] = useState('#32d142');
+  const [profitLossData , setProfitLossData] = useState(0);
+  const [profitLossPercentData, setProfitLossPercentData] = useState(0);
+  const [profitLoss, setProfitLoss] = useState(0);
+  const [profitLossPercent, setProfitLossPercent] = useState(0);
+  const dataRef = useRef(data);
+  const profitLossDataRef = useRef(profitLossData);
+  const profitLossPercentDataRef = useRef(profitLossPercentData);
+  const [timeoutRef, setTimeoutRef] = useState(null);
 
-  function getCurrentDate() {
-    const date = new Date();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const year = date.getFullYear();
-  
-    return `${year}-${month}-${day}`;
-  }
 
   const loadData = async () => {
-    const token = await Store.get('token');
-    
-    const config = {
-      'date_end': getCurrentDate(),
-      "period": "all",
-      "extended_hours": false
-    }
-    const headers = { headers: { Authorization: `Bearer ${token}` } }
-    
-    const data = await axios.post('http://192.168.1.9:8080/user/setup', config, headers)
-    setBaseValue(data.data.baseValue);
-    setEquity(parseFloat(data.data.equity[data.data.equity.length-1].toFixed(2)));
-    const filteredData = filterDataByTimeframe(data.data, timeframe);
-    console.log(filteredData)
+    const filteredData = await filterDataByPeriod();
+    setEquity(parseFloat(filteredData[filteredData.length-1].value.toFixed(2)));
     if (filteredData[filteredData.length-1].value >= filteredData[0].value) {
       setLineChartColor('#32d142');
     } else {
       setLineChartColor('#ff3903');
     }
-    const filledData = fillMissingData(filteredData, timeframe);
-    setData(filledData);
+    setData(filteredData);
   }
 
   useEffect(() => { 
     loadData();
-  }, [timeframe]);
+  }, [period]);
+
+  useEffect(() => {
+    dataRef.current = data;
+  }, [data]); 
+
+  useEffect(() => {
+    profitLossDataRef.current = profitLossData;
+  }, [profitLossData]);
+
+  useEffect(() => {
+    profitLossPercentDataRef.current = profitLossPercentData;
+  }, [profitLossPercentData]);
+
 
   const handleTimeframePress = (newTimeframe) => {
-    setTimeframe(newTimeframe);
+    setPeriod(newTimeframe);
   };
 
-  const handleAddCreatorPress = () => {
-    // Empty onPress handler for the "+" button
-  };
-
-  const filterDataByTimeframe = (data, timeframe) => {
+  const filterDataByPeriod = async () => {
+    // Figure out the cutoff date base on what period is selected
     const now = new Date().getTime() / 1000;
-    let cutoff;
-  
-    switch (timeframe) {
+    let timeframe;
+    switch (period) {
       case '1D':
-        cutoff = now - 24 * 60 * 60;
+        timeframe = '5Min';
         break;
-      case '3D':
-        cutoff = now - 3 * 24 * 60 * 60;
-        break;
-      case '7D':
-        cutoff = now - 7 * 24 * 60 * 60;
+      case '1W':
+        timeframe = '15Min';
         break;
       case '1M':
-        cutoff = now - 30 * 24 * 60 * 60;
+        timeframe = '1D';
         break;
       case '3M':
-        cutoff = now - 3 * 30 * 24 * 60 * 60;
+        timeframe = '1D';
         break;
-      case '6M':
-        cutoff = now - 6 * 30 * 24 * 60 * 60;
+      case '1Y':
+        timeframe = '1D';
         break;
       case 'All':
+        timeframe = '1D';
       default:
-        cutoff = -Infinity;
     }
-    
-    const filteredTimestamps = data.timestamp.filter((timestamp) => timestamp >= cutoff);
-    const filteredEquity = data.equity.filter((_, index) => data.timestamp[index] >= cutoff);
-    const result = filteredTimestamps.map((timestamp, index) => {
+    // calculate end date
+    const nowFormatted = new Date(now*1000).toISOString().slice(0, 10);
+    // setup config for the request
+    const token = await Store.get('token');
+    const config = {
+      'date_end': nowFormatted,
+      "period": period === '1Y' ? '1A' : period,
+      "timeframe": timeframe,
+      "extended_hours": false
+    }
+    const headers = { headers: { Authorization: `Bearer ${token}` } }
+    // make request
+    const response = await axios.post('http://192.168.1.9:8080/user/setup', config, headers)
+    // format data
+    setProfitLossData(response.data.profit_loss);
+    setProfitLossPercentData(response.data.profit_loss_pct);
+    const result = response.data.timestamp.map((timestamp, index) => {
       return {
         timestamp: timestamp,
-        value: filteredEquity[index],
+        value: response.data.equity[index],
       };
-    });
+    }).filter(item => item.value !== null);
     return result;
   };
-
-  function fillMissingData(data, timeframe) {
-    const startDate = data[0].timestamp * 1000; // Convert to milliseconds
-    const endDate = data[data.length - 1].timestamp * 1000;
-  
-    const newData = [];
-  
-    const oneDay = 24 * 60 * 60 * 1000; // Milliseconds in one day
-  
-    let currentIndex = 0;
-  
-    for (let currentDate = startDate; currentDate <= endDate; currentDate += oneDay) {
-      if (currentIndex < data.length - 1 && currentDate >= data[currentIndex + 1].timestamp * 1000) {
-        currentIndex++;
-      }
-  
-      if (timeframe === '1M' && new Date(currentDate).getMonth() !== new Date(data[currentIndex].timestamp * 1000).getMonth()) {
-        continue;
-      }
-  
-      newData.push({
-        timestamp: Math.floor(currentDate / 1000),
-        value: data[currentIndex].value
-      });
-    }
-  
-    return newData;
-  }
 
 const invokeHaptic = () => {
   haptics.impactAsync(haptics.ImpactFeedbackStyle.Light);
 }
 
+
 const endTouch = () => {
   setEquity(parseFloat(data[data.length-1].value.toFixed(2)));
+  setProfitLoss(0)
+  setProfitLossPercent(0)
   haptics.impactAsync(haptics.ImpactFeedbackStyle.Light);
 }
 
-function formatDateLong(timestamp) {
-  'worklet';
-  const date = new Date(timestamp*1000);
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const month = months[date.getMonth()];
-  const day = String(date.getDate()).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${month}, ${day} ${year}`;
+function formatDateLong(timestamp, period) {
+  'worklet'
+  let displayTime;
+  let date;
+  let months;
+  let month;
+  let day;
+  let year;
+  let hours;
+  let minutes;
+  let ampm;
+  let formattedHours;
+  let formattedMinutes;
+  switch (period) {
+    case '1D':
+      date = new Date(timestamp*1000);
+      hours = date.getHours();
+      minutes = date.getMinutes();
+      ampm = hours >= 12 ? 'PM' : 'AM';
+      formattedHours = hours % 12 || 12;
+      formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+      displayTime = `${formattedHours}:${formattedMinutes} ${ampm}`;
+      break;
+    case '1W':
+      date = new Date(timestamp*1000);
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      hours = date.getHours();
+      minutes = date.getMinutes();
+      ampm = hours >= 12 ? 'PM' : 'AM';
+      formattedHours = hours % 12 || 12;
+      formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+      displayTime = `${formattedHours}:${formattedMinutes} ${ampm}, ${months[date.getMonth()]} ${date.getDate()}`;;
+      break;
+    case '1M':
+      date = new Date(timestamp*1000);
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = months[date.getMonth()];
+      day = String(date.getDate()).padStart(2, '0');
+      year = date.getFullYear();
+      displayTime = `${month}, ${day} ${year}`;
+      break;
+    case '3M':
+      date = new Date(timestamp*1000);
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = months[date.getMonth()];
+      day = String(date.getDate()).padStart(2, '0');
+      year = date.getFullYear();
+      displayTime = `${month}, ${day} ${year}`;
+      break;
+    case '1Y':
+      date = new Date(timestamp*1000);
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = months[date.getMonth()];
+      day = String(date.getDate()).padStart(2, '0');
+      year = date.getFullYear();
+      displayTime = `${month}, ${day} ${year}`;
+      break;
+    case 'All':
+      date = new Date(timestamp*1000);
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = months[date.getMonth()];
+      day = String(date.getDate()).padStart(2, '0');
+      year = date.getFullYear();
+      displayTime = `${month}, ${day} ${year}`;
+    default:
+      
+  }
+  return displayTime;
 }
 
-const onCurrentIndexChange = (index) => {
-  setEquity(parseFloat(data[index].value.toFixed(2)));
-}
+const onLongPressHandlerStateChange = ({ nativeEvent }) => {
+  if (nativeEvent.state === State.ACTIVE) {
+    invokeHaptic();
+  } else if (nativeEvent.state === State.END) {
+    endTouch();
+  }
+};
 
+const onCurrentIndexChange = useCallback(
+  (index) => {
+    setEquity(parseFloat(dataRef.current[index].value.toFixed(2)));
+    setProfitLoss(parseFloat(profitLossDataRef.current[index].toFixed(2)));
+    setProfitLossPercent(parseFloat((profitLossPercentDataRef.current[index]*100).toFixed(2)));
+  },
+  []
+);
   return (
     <SafeAreaView style={styles.wrapper}>
       <StatusBar barStyle="dark-content" />
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.valueBox}>
-      <Text style={styles.totalValueTitle}>Investing</Text>
+      <Text style={styles.totalValueTitle}>Portfolio</Text>
       <Text style={styles.totalValue}>${equity}</Text>
+      {profitLossPercent !== 0 ? <Text style={[profitLossPercent < 0 ? {color: '#ff3903'} : {color: '#32d142'}]}>${Math.abs(profitLoss)} {Math.abs(profitLossPercent)}%</Text> : null}
       </View>
         <View style={styles.bottomContainer}>
             <View style={styles.graphContainer}>
-              {data.length === 0 ? <Text style={styles.emptyText}>No data to display</Text> : <LineChart.Provider data={data} onCurrentIndexChange={onCurrentIndexChange}>
+              {data.length === 0 ? <Text style={styles.emptyText}>No data to display</Text> :
+               <LineChart.Provider data={data} onCurrentIndexChange={onCurrentIndexChange}>
               <LineChart width={Dimensions.get('window').width} height={150}>
-                <LineChart.Path color={LineChartColor}/>
-                <LineChart.CursorLine />
-                <LineChart.CursorCrosshair onActivated={invokeHaptic} onEnded={endTouch}>
-                <LineChart.Tooltip position="bottom">
-                  <LineChart.DatetimeText format={({value}) => {'worklet'; 
-                  const newdate = formatDateLong(value)
-                  return newdate}}/>
+                <LineChart.Path color={LineChartColor}></LineChart.Path>
+                <LineChart.CursorLine/>
+                <LineChart.CursorCrosshair color={LineChartColor} onActivated={invokeHaptic} onEnded={endTouch} minDurationMs={200}>
+                <LineChart.Tooltip cursorGutter={100} position="top">
+                  <LineChart.DatetimeText format={({value}) => {
+                    'worklet'
+                    const dateval = formatDateLong(value, period)
+                    return dateval}}/>
                 </LineChart.Tooltip>
               </LineChart.CursorCrosshair>
               </LineChart>
-            </LineChart.Provider>}
+            </LineChart.Provider>
+            }
+            
               </View>
-              <View style={styles.timeframeTabs}>
-                {['1D', '3D', '7D', '1M', '3M', '6M', 'All'].map((tf) => (
-                  <TouchableOpacity key={tf} style={[{ borderRadius: 5 }, timeframe === tf ? styles.activeTimeframe && {backgroundColor: LineChartColor}: styles.activeTimeframe && {backgroundColor: "transparent"}]} onPress={() => handleTimeframePress(tf)}>
-                    <Text style={[styles.timeframeText, timeframe === tf ? { color: "#fff"} : {color: LineChartColor}]}>{tf}</Text>
+              <View style={styles.periodTabs}>
+                {['1D', '1W', '1M', '3M', '1Y', 'All'].map((tf) => (
+                  <TouchableOpacity key={tf} style={[{ borderRadius: 5 }, period === tf ? styles.activeTimeframe && {backgroundColor: LineChartColor}: styles.activeTimeframe && {backgroundColor: "transparent"}]} onPress={() => handleTimeframePress(tf)}>
+                    <Text style={[styles.periodText, period === tf ? { color: "#fff"} : {color: LineChartColor}]}>{tf}</Text>
                   </TouchableOpacity>
                 ))}
               </View>
-        
       <View style={styles.separator} />
-
       <View style={styles.creatorList}>
-        {/* Replace this array with your actual list of creators */}
         {Array(3).fill().map((_, i) => (
           <View key={i} style={styles.creatorCard}>
             <View style={styles.profile}>
-              {/* Replace this View with an Image component for the profile picture */}
+              
               <View style={styles.profilePicture} />
               <Text style={styles.username}>Username {i + 1}</Text>
             </View>
@@ -207,7 +260,7 @@ const onCurrentIndexChange = (index) => {
           </View>
         ))}
         <View style={styles.creatorCard}>
-        <TouchableOpacity onPress={handleAddCreatorPress} style={styles.addButton}>
+        <TouchableOpacity onPress={() => {}} style={styles.addButton}>
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
         </View>
@@ -219,6 +272,9 @@ const onCurrentIndexChange = (index) => {
 };
 
 const styles = StyleSheet.create({
+  diffText: {
+
+  },
   valueBox: {
     flex: 1,
     flexDirection: 'column',
@@ -277,7 +333,7 @@ const styles = StyleSheet.create({
   separator: {
     height: 1,
     width: '80%',
-    backgroundColor: '#c76982',
+    backgroundColor: 'lightgray',
     marginVertical: 20,
   },
   graphPlaceholder: {
@@ -288,13 +344,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-    timeframeTabs: {
+    periodTabs: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
     paddingHorizontal: 50,
     },
-    timeframeText: {
+    periodText: {
     borderRadius: 5,
     padding: 4,
     fontSize: 16,
